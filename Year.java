846e1f5d9b219ec33c82bcca96323a8f703bff55
Sarnala-11 <yaminisarnala@gmail.com>
2023-11-03 12:39:17 +0530

    Update this Year file

--- Year.java
+++ Year.java
@@ -2,7 +2,7 @@ class Year
 {
    public static void main(String args[])
    {
-	int a=2002;
+	int a=2000;
 	System.out.println((a%4==0 && a%100!=0 || a%400==0)?"It is a leap year  \n" + "Increment = "  + a++:"It is not a leap year  \n"  + "Increment = " + ++a);
    }
 }
